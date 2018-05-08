45844f0ffc012e9642e6f2c91a0b9f4f0bd149cc
Jan Simbera <simbera.jan@gmail.com>
2018-05-08 12:13:41 +0200

    attribute recategorizer

---
+++ recategorize.py
@@ -0,0 +1,23 @@
+import rum
+import rum.util
+
+DESCRIPTION = '''Recategorizes a categorical attribute.
+
+Given a column in a defined schema table, creates its copy with values
+recategorized using given translation rules.
+'''
+
+argparser = rum.defaultArgumentParser(DESCRIPTION)
+argparser.add_argument('table', help='table containing the attribute')
+argparser.add_argument('source', help='the attribute column to be recategorized')
+argparser.add_argument('target', help='the column to create and put the result in')
+argparser.add_argument('translation', help='a JSON file defining the translation')
+argparser.add_argument('-o', '--overwrite', action='store_true',
+    help='overwrite existing table'
+)
+
+if __name__ == '__main__':
+    args = argparser.parse_args()
+    rum.util.Recategorizer.fromArgs(args).run(
+        args.table, args.source, args.target, args.translation, args.overwrite
+    )
---
+++ rum/util.py
@@ -0,0 +1,70 @@
+import json
+from psycopg2 import sql
+
+from . import core
+
+class PassThrough:
+    pass
+
+class Recategorizer(core.DatabaseTask):
+    def main(self, table, source, target, translation, overwrite=False):
+        transDict, default, leading = self.loadTranslation(translation)
+        targetType = core.TYPES_TO_POSTGRE[type(next(iter(transDict.values())))]
+        with self._connect() as cur:
+            self.createTargetField(cur, table, target, targetType, overwrite)
+            qry = sql.SQL('UPDATE {schema}.{table} SET {target} = ').format(
+                schema=self.schemaSQL,
+                table=sql.Identifier(table),
+                target=sql.Identifier(target),
+            ) + self.caseQuery(cur, source, transDict, default, leading)
+            self.logger.debug('recategorizing with %s', qry)
+            cur.execute(qry)
+        
+    def loadTranslation(self, transPath):
+        with open(transPath, encoding='utf8') as infile:
+            transDict = json.load(infile)
+        if 'translation' in transDict:
+            return (
+                transDict['translation'],
+                transDict.get('default', PassThrough),
+                bool(transDict.get('leading', False))
+            )
+        else:
+            return transDict, PassThrough, False
+        
+    def createTargetField(self, cur, table, colname, coltype, overwrite):
+        opts = dict(
+            schema=self.schemaSQL,
+            table=sql.Identifier(table),
+            colname=sql.Identifier(colname)
+        )
+        if overwrite:
+            delqry = sql.SQL(
+                'ALTER TABLE {schema}.{table} DROP COLUMN IF EXISTS {colname}'
+            ).format(**opts)
+            self.logger.debug('dropping target column: %s', delqry)
+            cur.execute(delqry)
+        qry = sql.SQL('''ALTER TABLE {schema}.{table}
+            ADD COLUMN {colname} {coltype}''').format(
+            coltype=sql.SQL(coltype), **opts
+        ).as_string(cur)
+        self.logger.debug('adding recategorized column: %s', qry)
+        cur.execute(qry)
+        
+    def caseQuery(self, cur, column, translation, default=PassThrough, leading=False):
+        colSQL = sql.Identifier(column)
+        cases = [
+            sql.SQL('WHEN {column} {matcher} {fromval} THEN {toval}').format(
+                column=colSQL,
+                matcher=sql.SQL('LIKE' if leading else '='),
+                fromval=sql.Literal((fromval + '%') if leading else fromval),
+                toval=sql.Literal(toval),
+            )
+            for fromval, toval in translation.items()
+        ]
+        defaultSQL = colSQL if default is PassThrough else sql.Literal(default)
+        return (
+            sql.SQL('CASE\n') + 
+            sql.SQL('\n').join(cases) + 
+            sql.SQL('\nELSE {} END').format(defaultSQL)
+        )
